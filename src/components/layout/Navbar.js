88e25947091060aed6d5e7c0219c1797a224d345
Leelsmuth <femianjorin@rocketmail.com>
2020-03-04 10:44:42 +0100

    navbar refactor

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,14 +4,17 @@ import { NavLink } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <NavLink className="navbar-brand" to="/">Home</NavLink>
+      {/* <NavLink className="navbar-brand" to="/">Home</NavLink>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
-      </button>
+      </button> */}
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">Home</NavLink>
+          </li>
+          <li className="nav-item">
             <NavLink className="nav-link" to="/register">Register</NavLink>
           </li>
           <li className="nav-item">
